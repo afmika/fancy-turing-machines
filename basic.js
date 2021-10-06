57362f2e6c8489c10ecf44de35fa188edde3bc81
afmika <afmichael73@gmail.com>
2021-10-06 09:17:47 +0300

    machine generator refactored

--- basic.js
+++ basic.js
@@ -1,53 +1,11 @@
-const Machine = require('./classes/Machine');
-
-function generateMachineFrom(str) {
-	const transitions = [];
-
-	str = str.replace(' ', '_');
-
-	for (let i = 0; i < str.length; i++) {
-		const ch = str[i];
-		transitions.push({
-			current: {
-				state: 'q' + i,
-				reads: Machine.BLANK
-			},
-			transitTo: {
-				state: 'q' + (i + 1),
-				writes: ch,
-				go: Machine.RIGHT
-			}
-		});
-	}
-
-	let L = str.length - 1;
-	transitions.push({
-		current: {
-			state: 'q' + (L + 1),
-			reads: Machine.BLANK
-		},
-		transitTo: {
-			state: 'q' + (L + 2),
-			writes: Machine.BLANK,
-			go: Machine.HALT
-		}
-	});
-
-	const machine = new Machine('Test');
-	machine.transitions = transitions;
-	machine.initialState = 'q0';
-	machine.acceptedState = 'q1' + (L + 2);
-
-	return machine;
-}
+const MachineGenerator = require("./classes/MachineGenerator");
 
 // plz use std-out
-const machine = generateMachineFrom ('Mika');
+const machine = MachineGenerator.fromString ('Mika');
 // const code = machine.asCode();
 // console.log(code);
 
 machine.compile ();
-// machine.setInput ('RAZAFY BE');
 machine.show ();
 
 let interval = setInterval (() => {
@@ -55,7 +13,9 @@ let interval = setInterval (() => {
 		machine.show ();
 		if (verdict || error) {
 			clearInterval (interval);
-			console.log(verdict, 'curr_pos = ' + cursor_pos, error || 'No error');
+			console.log(verdict, 'curr_pos = ' + cursor_pos);
+			if (error) 
+				console.error (error.message);
 		}
 	});
 }, 500);
---
+++ classes/MachineGenerator.js
@@ -0,0 +1,52 @@
+const Constant = require('./Constant');
+const Machine = require('./Machine');
+
+module.exports = class MachineGenerator {
+    /**
+     * Creates a non-compiled working Turing Machine that displays a given string of any length
+     * @param {string} str 
+     * @returns 
+     */
+    static fromString (str) {
+        const transitions = [];
+    
+        str = str.replace(' ', '_');
+    
+        for (let i = 0; i < str.length; i++) {
+            const ch = str[i];
+            transitions.push({
+                current: {
+                    state: 'q' + i,
+                    reads: Constant.BLANK
+                },
+                transitTo: {
+                    state: 'q' + (i + 1),
+                    writes: ch,
+                    go: Constant.RIGHT
+                }
+            });
+        }
+    
+        let L = str.length - 1;
+        transitions.push({
+            current: {
+                state: 'q' + (L + 1),
+                reads: Constant.BLANK
+            },
+            transitTo: {
+                state: 'q' + (L + 2),
+                writes: Constant.BLANK,
+                go: Constant.HALT
+            }
+        });
+
+        console.log(transitions)
+    
+        const machine = new Machine('Test');
+        machine.transitions = transitions;
+        machine.initialState = 'q0';
+        machine.acceptedState = 'q1' + (L + 2);
+    
+        return machine;
+    }
+}
